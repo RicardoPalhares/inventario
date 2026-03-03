9bcb4e83e0bd7b616bfa3b5df2dfd964638adae3
o5lide <isaac.assis2510@gmail.com>
2026-03-03 14:50:54 -0300

    Paradinha legal
    Codiguin atualizado

--- main.cpp
+++ main.cpp
@@ -1,25 +1,35 @@
 // Leonardo Lopes Prado - 809
-// Gabriel GregÃ³rio de Olveira - 2324
+// Gabriel Gregório de Olveira - 2324
 // Isaac Assis Costa Lopes - 2314 
 // Ricardo de Oliveira Palhares - 2294
 // Gabriel Genghini Craveiro - 2274
 // Marcus Vinicius Duarte - 2296
 
 #include <iostream>
+#include <string>
 #include "opcoes_menu.h"
 
 using namespace std;
 
 int main(){
     int opcao;
-
-
-    mostrar_opcoes();
-
-    while(true)
-    {
-    cin >> opcao;
-    switch(opcao){
+	
+	while(true)
+	{
+		
+		mostrar_opcoes();	
+    	
+    	if (!(cin >> opcao)) {
+    		cout << endl;
+            cout << "Por favor, digite um numero!" << endl;
+            cout << endl;
+            cin.clear(); // Limpa o estado de erro do cin
+            cin.ignore(1000, '\n'); // Descarta a entrada inválida
+            continue;
+        }
+    	
+		switch(opcao)
+		{
             case 1: inserir_item(); break;
             case 2: cadastrar_similaridade(); break;
             case 3: buscar_similares(); break;
@@ -29,9 +39,12 @@ int main(){
             case 7: contar_propriedade(); break;
             case 8: remover_menos_raros(); break;
             case 9: return 0;
-            default: cout << "Opcao invalida!" << endl;
-            
-    }
-    }
+            default: cout << endl; 
+			cout << "Opcao invalida!" << endl; 
+			cout << endl; break;
+    	}
+	}
+    
+ 
     return 0;
 }
