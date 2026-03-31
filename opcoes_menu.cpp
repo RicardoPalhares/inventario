3229b56cedef3ff9c206fdaf27192204b897dd31
Gabriel Gregório <106854433+gabrielgregori0@users.noreply.github.com>
2026-03-31 14:08:27 -0300

    Alteração função de inserção

--- opcoes_menu.cpp
+++ opcoes_menu.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <string>
-#include "opcoes_menu.h" 
 #include <list>
+#include "opcoes_menu.h" 
 
 using namespace std;
 
 void mostrar_opcoes(){
-		cout << "--- Bem vindo(a) ao inventario: Bolsa Devoradora ---" << endl;
         cout << "1. Inserir item" << endl;
         cout << "2. Cadastrar similaridade entre itens" << endl;
         cout << "3. Buscar itens similares" << endl;
@@ -20,6 +19,8 @@ void mostrar_opcoes(){
 }
 
 Item itens[1000];
+list<Aresta> grafo[1000];
+
 int id = 0;
 int N = 0;
 void inserir_item(){
@@ -37,37 +38,72 @@ void inserir_item(){
 	}
 	else{
 		itens[id] = {id, nome_item, dono, propriedade_magica, raridade};
-    	cout << nome_item << " inserido a Bolsa Devoradora" << endl;
+    	cout << nome_item << " inserido(a) na Bolsa Devoradora" << endl;
+    	cout << endl;
     	id++;
     	N++;
 	}
 }
 
 void cadastrar_similaridade(){
-    int vertices, peso;
+	int peso;
 	int id1, id2; //papel de  origem, destino
-	
-	vertices = N;
-
-	
-	list<Aresta> grafo[vertices];
-	
-
-	cout << "Inisra os ids dos item a serem cadastrados com similaridade: " << endl;
-	cin >> id1 >> id2;
+	string op;
 		
-	cout << "Insira o valor da similaridade: " << endl;
-	cin >> peso;
+	cout << "Insira os ids dos item a serem cadastrados com similaridade: " << endl;
+	cin >> id1 >> id2;
+	if((id1 < N && id1 >= 0) && (id2 < N && id2 >= 0)){
+		cout << "Itens a serem cadastrados: " << id1 << " -> " << itens[id1].nome << ", " << id2 << " -> " << itens[id2].nome << endl;
+		cout << "Tem certeza que deseja cadastrar esses itens? [y/n]" << endl;
+		cin >> op;
+		if(op == "y" || op == "Y"){
+			cout << "Insira o valor da similaridade entre eles: " << endl;
+			cin >> peso;
 	
-
-
-	grafo[id1].push_back({id1, id2, peso});
-	grafo[id2].push_back({id2, id1, peso});
+			grafo[id1].push_back({id1, id2, peso});
+			grafo[id2].push_back({id2, id1, peso});
+		}
+		else{
+			cout << "Se quiser realizar o cadastro entre dois itens aperte 2" << endl;
+		}
+	}
+	else if((id1 > N || id1 < 0) || (id2 > N || id2 < 0)){
+		cout << "Id invalido, voce deve inserir o valor para o id de 0 ate " << N - 1 << endl;
+	}
 }
 
-
 void buscar_similares(){
-    cout << "Funcionalidade em construcao..." << endl;
+    int C;      // id do item base
+    int x;      // similaridade minima
+    string j;   // jogador ignorado
+
+    cout << "Digite o id do item base: ";
+    cin >> C;
+
+    cout << "Digite o valor minimo de similaridade: ";
+    cin >> x;
+
+    cout << "Digite o nome do jogador que deve ser ignorado: ";
+    cin >> j;
+
+    list<Aresta>::iterator it;
+	bool encontrou = false;
+	
+    for(it = grafo[C].begin(); it != grafo[C].end(); it++){
+        int id_similar = it->id2;
+
+        if(it->peso > x){
+            if(itens[id_similar].dono != j){
+                cout << itens[id_similar].nome << ", ";
+                
+                encontrou = true;
+            }
+        }
+    }
+
+    if(encontrou == false){
+        cout << "Nenhum item encontrado com esses criterios." << endl;
+    }
 }
 
 void verificar_existencia(){
